7739807224ec772aea564ae6e528612b5a13e2b8
Nick Ayers <n1ckay3r5@gmail.com>
2019-10-25 11:10:51 +0100

    add apm, remove example visualizations

--- start-elk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -d --rm --name elasticsearch --net elastic -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.0.1
-
-docker run --rm -d --name kibana --net elastic -p 5601:5601 kibana:7.0.1
